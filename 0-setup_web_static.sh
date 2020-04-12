4a78b44f245a6374d3e5b8f3abb0488415e7ae1c
amine784 <1066@holbertonschool.com>
2020-04-12 16:06:03 +0100

    holberto

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,6 @@ sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 echo "first web page" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-location="\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "/server_name _;/ i $location" /etc/nginx/sites-available/default
+sudo sed -i "36 i\\tlocation /hbnb_static {\n\talias /data/web_static/current;\n}" /etc/nginx/sites-available/default
 sudo chown ubuntu:ubuntu -R /data/
 sudo service nginx restart
