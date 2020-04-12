475c98a975f60c0dc7ad551d6e607809b6ed159a
amine784 <1066@holbertonschool.com>
2020-04-12 15:18:07 +0100

    rect task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # script to prepare env
-sudo apt-get install update
-sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
+sudo chown ubuntu:ubuntu -R /data/
 echo "first web page" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '35 i\   \tlocation /hbnb_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
 sudo service nginx restart
