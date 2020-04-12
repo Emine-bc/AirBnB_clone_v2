ca89ba53b457a21191d65e3b3e43bdd680c188ca
amine784 <1066@holbertonschool.com>
2020-04-12 20:31:08 +0100

    holberto

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,19 +23,3 @@ def do_deploy(archive_path):
     '''func deploy'''
     if not os.path.exists(archive_path):
         return(False)
-    try:
-        put(archive_path, "/tmp/")
-        deploy_file = archive_path.split("/")[1].split(".")[0]
-        file_path = file_deploy.split(".")[0]
-        path = archive_path.split("/")[-1]
-        deploy_path = "/data/web_static/releases/{}".format(deploy_file)
-        run("mkdir -p {}".format(deploy_path))
-        run("tar -xzf /tmp/{} - C {}".format(path, deploy_path))
-        run("rm -rf /tmp/{}".format(path))
-        run("mv {}/web_static/* {}/".format(deploy_path, deploy_path))
-        run("rm -rf {}/web_static".format(deploy_path))
-        run("rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(deploy_path))
-        return(True)
-    except:
-        return(False)
