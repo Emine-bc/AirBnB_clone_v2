e36adfb998356184a548697376b6b140a40f34e4
amine784 <1066@holbertonschool.com>
2020-04-12 20:27:49 +0100

    holberto

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,21 +2,21 @@
 '''Fabric script that generates a .tgz archive from the contents
    of Airbnb_clone_v2
 '''
+
 from datetime import datetime
 from fabric.api import *
-import sys
-import os
+from time import strftime
 env.hosts = ["34.73.0.225", "35.229.78.70"]
 
 
 def do_pack():
     """function"""
     local("mkdir -p versions")
-    full = "versions/web_static_{}.tgz".format(
-        datetime.now.strftime("%Y%m%d%H%M%S"))
-    if full is None:
-        return(None)
-    return(local("tar -cvzf {} web_static".format(full)))
+    full = local("tar -cvzf versions/web_static_{}.tgz\
+        web_static".format(strftime("%Y%m%d%H%M%S")))
+    if full:
+        return ("versions/web_static_{}".format(strftime("%Y%m%d%H%M%S")))
+    return (None)
 
 
 def do_deploy(archive_path):
