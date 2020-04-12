dc8636adf68a83ce950e681371dd015efa4e0b6f
amine784 <1066@holbertonschool.com>
2020-04-12 15:23:18 +0100

    azert

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
-sudo chown ubuntu:ubuntu -R /data/
 echo "first web page" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo sed -i '35 i\   \tlocation /hbnb_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
+sudo chown ubuntu:ubuntu -R /data/
 sudo service nginx restart
