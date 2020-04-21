83c4c5f8b7444d0fd774db9ec52713af47b80d94
amine784 <1066@holbertonschool.com>
2020-04-21 20:02:00 +0100

    new pushh

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''script that create new route'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    '''“Hello HBNB!” '''
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    '''“HBNB!” '''
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_var_text(text):
+    '''“HBNB!” '''
+    return ("C " + str(text.replace("_", " ")))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def p_var_text(text):
+    '''“HBNB!” '''
+    return ("Python " + str(text.replace("_", " ")))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
