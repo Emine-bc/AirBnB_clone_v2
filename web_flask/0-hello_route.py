3df88fc99aecaac2337346d450bb9c1dec298668
amine784 <1066@holbertonschool.com>
2020-04-21 18:58:26 +0100

    holberto

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
     '''“Hello HBNB!” '''
-    return ("Hello HBNB!")
+    return ('Hello HBNB!')
 if __name__ == "__main__":
-    app.run("0. 0.0.0", port=5000)
+    app.run(host='0.0.0.0')
