7b7a7f6a232f9f22ae05e960093a3e5814c7404c
amine784 <1066@holbertonschool.com>
2020-04-21 19:16:31 +0100

    holberto

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''script that create new route'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    '''“Hello HBNB!” '''
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_world():
+    '''“HBNB!” '''
+    return ('HBNB!')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
