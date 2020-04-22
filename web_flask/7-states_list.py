672c987d2185bd90747ecb71cb0b649876ae3276
amine784 <1066@holbertonschool.com>
2020-04-22 16:48:55 +0100

    holberto

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''script that create new route'''
+from models import storage
 from flask import render_template
 from flask import Flask
 app = Flask(__name__)
@@ -7,15 +8,12 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def state_l():
-    '''“HBNB!” '''
+    '''HBNB!'''
     return (render_template(
         '7-states_list.html', states=storage.all('State').values()))
 
 
 @app.teardown_appcontext
-'''“HBNB!” '''
-
-
 def tear_d(self):
     storage.close()
 if __name__ == "__main__":
